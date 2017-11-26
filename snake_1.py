8ab2c2dbc776d210d04d119ce12556e9d57ace78
Denestel <denestel@gmail.com>
2017-11-26 10:52:00 -0600

    almost done

--- snake_1.py
+++ snake_1.py
@@ -12,15 +12,72 @@ SNAKE_X = SNAKE_LENGTH+1
 SNAKE_Y = 3
 TIMEOUT = 100
 
-
 class Snake(object):
     """docstring for Snake."""
-    def __init__(self):
-        slef.x = "Hisss!"
 
-    def method_a(self, foo):
-        print (self.x + ' ' + foo)
+    #This is the dictionary for key inputs
+    #reverse direction map
+    REV_DIR_MAP = {
+        KEY_UP: KEY_DOWN,
+        KEY_DOWN: KEY_UP,
+        KEY_LEFT: KEY_RIGHT,
+        KEY_RIGHT: KEY_LEFT
+    }
 
+    def __init__(self, x, y, window):
+        self.body_list = []
+        self.hit_score = 0
+        self.timeout = TIMEOUT
 
-snake = Snake();
-snake.method_a("Says the snake")
+        for element in range(SNAKE_LENGTH, 0, -1):
+            self.body_list.append(Body(x-element, y))
+
+        #Define and append the snakes head
+        self.body_list.append(Body(x, y, '0'))
+        #Define the window
+        self.window = window
+        #Move the snake to the right when the game starts
+        self.direction = KEY_RIGHT
+        #Set the snakes last head coordinate
+        self.last_head_coor = (x, y)
+        #Define the direction map
+        self.direction_map = {
+            KEY_UP: self.move_up,
+            KEY_DOWN: self.move_down,
+            KEY_LEFT: self.move_left,
+            KEY_RIGHT: self.move_right
+        }
+
+    @property
+    def score(self):
+        return ("Score: {}".format(self.hit_score))
+
+class Body(object):
+    """docstring for Body."""
+
+    #Initialize the body
+    def __init__(self, x, y, char = '='):
+        self.x = x
+        self.y = y
+        self.char = char
+
+    @property
+    #Set the coordinate
+    def coor(self):
+        return self.x, self.y
+
+class Food(object):
+    #Initialize Food
+    def __init__(self, window, char = '*'):
+        #Have food appear in a random position
+        self.x = randint(1, MAX_X)
+        self.y = randint(1, MAX_Y)
+        self.char = char
+        self.window = window
+
+    def render(self):
+        self.window.addstr(self.y, self.x, self.char)
+
+    def reset(self):
+        self.x = randint(1, MAX_X)
+        self.y = randint(1, MAX_Y)
